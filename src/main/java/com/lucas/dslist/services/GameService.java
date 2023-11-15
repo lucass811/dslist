d6f0392a128743c356abb82d220d7f3647d1588b
Lucas <lucas.desenvolvedor@hotmail.com>
2023-11-15 19:07:34 -0300

    GET game by id

--- src/main/java/com/lucas/dslist/services/GameService.java
+++ src/main/java/com/lucas/dslist/services/GameService.java
@@ -4,16 +4,24 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.lucas.dslist.dto.GameDTO;
 import com.lucas.dslist.dto.GameMinDTO;
 import com.lucas.dslist.entities.Game;
 import com.lucas.dslist.repositories.GameRepository;
+
 
 @Service
 public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
     
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
