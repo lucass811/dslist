d01a3ee726db3cbc77426152152bcee46664b4ee
Lucas <lucas.desenvolvedor@hotmail.com>
2023-11-15 15:07:20 -0300

    Domain model

--- src/main/java/com/lucas/dslist/controllers/GameController.java
+++ src/main/java/com/lucas/dslist/controllers/GameController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucas.dslist.dto.GameMinDTO;
-import com.lucas.dslist.entities.Game;
 import com.lucas.dslist.services.GameService;
 
 @RestController
